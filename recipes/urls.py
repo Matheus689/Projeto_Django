e9ea05dc1dd2430f829adde57c1587a9cc98b99f
Matheus <matheusbp.6889@gmail.com>
2025-08-12 20:57:02 -0300

    aula 3 ( tudo certin )

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,3 +5,11 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
 ]
+
+# recipes/urls.py
+from django.urls import path # Importa a função 'path' que define URLs.
+from . import views # Importa o arquivo views.py do próprio app.
+
+urlpatterns = [
+    path('', views.recipe_list, name='recipe_list'), # nova view para listar
+]
