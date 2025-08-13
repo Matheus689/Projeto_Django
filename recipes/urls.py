3046f912a3d0f551572f50c92f798bb57e9baddd
Matheus <matheusbp.6889@gmail.com>
2025-08-12 21:12:45 -0300

    aula 4 add de iamgem na lista ( tudo certo )

--- recipes/urls.py
+++ recipes/urls.py
@@ -13,3 +13,12 @@ from . import views # Importa o arquivo views.py do próprio app.
 urlpatterns = [
     path('', views.recipe_list, name='recipe_list'), # nova view para listar
 ]
+# recipes/urls.py
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.recipe_list, name='recipe_list'),
+    # Esta linha é crucial: define a URL e nomeia ela como 'recipe_detail'
+    path('<int:pk>/', views.recipe_detail, name='recipe_detail'),
+    # Outras URLs do seu aplicativo...
+]
